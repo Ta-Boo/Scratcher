06a618b1889ff55efd8dd18458db9ac92d0fe0c5
Tobiáš Hládek <tobias.hladek1@gmail.com>
2025-11-09 12:30:53 +0100

    Initial Commit

---
+++ Scratcher/ScratcherApp.swift
@@ -0,0 +1,13 @@
+//Created by Tobiáš Hládek on 09/11/2025.
+// 
+
+import SwiftUI
+
+@main
+struct ScratcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
